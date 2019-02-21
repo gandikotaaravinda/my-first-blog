015fb530df9b47f4b6a70412768934eef5f2f698
gandikotaaravinda <47440651+gandikotaaravinda@users.noreply.github.com>
2019-02-21 10:20:00 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url, include
-from django.contrib.auth import views as auth_views
+#from django.contrib.auth import views as auth_views
 from django.urls import path 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^signup/$', views.signup, name='signup'),
-    url(r'^account_activation_sent/$', views.account_activation_sent, name='account_activation_sent'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
+   # url(r'^$', views.home, name='home'),
+    #url(r'^signup/$', views.signup, name='signup'),
+    #url(r'^account_activation_sent/$', views.account_activation_sent, name='account_activation_sent'),
+    #url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
